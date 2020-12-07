acdb1db9447f9ea2bef673ab21a746cd1349c016
TalB <tal.burdman@gmail.com>
2020-12-07 12:30:21 +0100

    creacion rama

--- client/src/components/pages/Petitions/Petition-card/Petition-card.js
+++ client/src/components/pages/Petitions/Petition-card/Petition-card.js
@@ -12,7 +12,7 @@ const PetitionCard = ({_id, image, title, description}) => {
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>{description}</Card.Text>
 
-                    <Link className="btn btn-dark btn-block btn-sm" to={`/petitions/${_id}`}>Ver detalles</Link>
+                    <Link className="btn btn-dark btn-block" to={`/petitions/${_id}`}>Ver detalles</Link>
 
                 </Card.Body>
             </Card>
